a7ccecc593df87f3d3d032e61100cfc4df5e691b
vaibhavpatel <vaibhavP@agilebrainsconsulting.com>
2024-04-23 19:49:11 +0530

    add-product changes

--- src/app/api/add-product/route.ts
+++ src/app/api/add-product/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
+import Replicate from "replicate";
 import Product from "../../../../models/Product";
 import { connectDb } from "@/app/utils/db";
 import { uploadFileToS3 } from "@/app/utils/s3";
+
+const replicate = new Replicate();
 
 export async function POST(request: Request) {
   try {
@@ -19,15 +22,16 @@ export async function POST(request: Request) {
     const output = await replicate.run(process.env.REPLICATE_EMBEDDED_KEY, {
       input,
     });
+
     const createProduct = new Product({
       name: fileName,
       url: imageUrl,
       embadding: output,
     });
-
     await createProduct.save();
     return NextResponse.json({ message: "Product created successfully" });
   } catch (err) {
+    console.log("err", err);
     return NextResponse.json({
       message: "Something went wrong, please try again",
     });
