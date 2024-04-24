d751c1fc6df7c66c3afb62aab3fb66fcebee4cbe
vaibhavpatel <vaibhavP@agilebrainsconsulting.com>
2024-04-24 19:01:51 +0530

    AWS env variable changes

--- src/app/utils/s3.js
+++ src/app/utils/s3.js
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 import fs from "fs/promises";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 
